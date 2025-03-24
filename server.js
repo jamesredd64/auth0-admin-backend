1d47ecd6e1b7a6fd8cdbec876ed68dd6f7328a6a
jamesredd64 <jredd2013@gmail.com>
2025-03-23 23:41:29 -0400

    cors 4

--- server.js
+++ server.js
@@ -18,13 +18,16 @@ app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
     "default-src 'self'; " +
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; " +
     "font-src 'self' https://fonts.gstatic.com data:; " +
     "img-src 'self' data: https:; " +
     "connect-src 'self' https://*;"
   );
   next();
 });
+// <meta http-equiv="Content-Security-Policy" content="style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;">
+
+
 
 app.use(cors({
   origin: (origin, callback) => {
